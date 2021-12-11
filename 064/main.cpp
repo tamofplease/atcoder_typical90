6a01b69caa01e1803ad28f868d7b6335417741b4
tamofplease <riddimdubstep@yahoo.co.jp>
2021-12-12 00:40:20 +0900

    solve 064 with cpp

--- 064/main.cpp
+++ 064/main.cpp
@@ -8,20 +8,31 @@ int N, Q;
 const int MAXN = 110000;
 const int MAXQ = 110000;
 ll A[MAXN];
+ll B[MAXN];
 int L[MAXQ], R[MAXQ], V[MAXQ];
 void input() {
     cin >> N >> Q;
-    rep(i, N) cin >> A[i];
-    rep(i, Q) cin >> L[i] >> R[i] >> V[i];
+    Rep(i, N) cin >> A[i];
+    Rep(i, Q) cin >> L[i] >> R[i] >> V[i];
 }
 
 void solve() {
-    ll res = 0;
-    rep(i, N-1) res += abs(A[i] - A[i+1]);
-
-    rep(i, Q) {
-        
+    ll tmp = 0;
+    Rep(i, N - 1) {
+        B[i] = A[i+1] - A[i];
+        tmp += abs(B[i]);
     }
+
+    Rep(i, Q) {
+        ll before = abs(B[L[i] - 1]) + abs(B[R[i]]);
+        if(L[i] >= 2) B[L[i] - 1] += V[i];
+        if(R[i] < N ) B[R[i]] -= V[i];
+        ll after = abs(B[L[i] - 1]) + abs(B[R[i]]);
+        tmp += (after - before);
+        cout << tmp << endl;
+    }
+
+    
 }
 
 int main(){
